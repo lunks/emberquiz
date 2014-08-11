b34cc58866a2d18b09356aa0b82577fd400e783a
Pedro Nascimento <pnascimento@gmail.com>
2014-08-11 17:16:32 -0300

    Adding seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Card.count.zero?
+  5.times do |n|
+    Card.create question: "Question number #{n}", answer: n
+  end
+end
